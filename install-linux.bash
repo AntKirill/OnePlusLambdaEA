f9748d49d0e59d084b9373c8456757c7ba514158
kant97 <kirant9797@gmail.com>
2018-03-08 22:40:40 +0300

    add install script

---
+++ install-linux.bash
@@ -0,0 +1,28 @@
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+
+sudo apt-get install -qq g++-5
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
+
+CMAKE_VERSION=3.3.2
+CMAKE_VERSION_DIR=v3.3
+
+CMAKE_OS=Linux-x86_64
+CMAKE_TAR=cmake-$CMAKE_VERSION-$CMAKE_OS.tar.gz
+CMAKE_URL=http://www.cmake.org/files/$CMAKE_VERSION_DIR/$CMAKE_TAR
+CMAKE_DIR=$(pwd)/cmake-$CMAKE_VERSION
+
+wget --quiet $CMAKE_URL
+mkdir -p $CMAKE_DIR
+tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
+export PATH=$CMAKE_DIR/bin:$PATH
+
+wget boost_1_66_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.66.0/boost_1_66_0.tar.gz/download
+tar xzvf boost_1_66_0.tar.gz
+./boost_1_66_0/bootstrap.sh --with-libraries=system
+./boost_1_66_0/b2 link=shared threading=multi variant=release
+
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo apt-get update -qq
+sudo apt-get install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
+sudo apt-get install qt5-default qttools5-dev-tools
