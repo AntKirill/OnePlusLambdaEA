290a317c4839ff8596db3b071a585f1a9e292ade
kant97 <kirant9797@gmail.com>
2018-04-03 09:22:41 +0300

    refactored chart building

--- src/chartBuilding/chart_builder_main.cpp
+++ src/chartBuilding/chart_builder_main.cpp
@@ -6,6 +6,7 @@
 #include <unordered_set>
 #include <string>
 #include <vector>
+#include "resultsTableScanner.h"
 
 void printUsage(const std::string &s = "")
 {
@@ -17,16 +18,6 @@ void printUsage(const std::string &s = "")
     std::cout << "List all files that you want to parse\n";
 }
 
-struct Table
-{
-    std::string name;
-    std::map<uint, std::map<uint, std::pair<uint, double>>> data;
-    QColor color;
-    Table(const std::string &name, const QColor &color) : name(name), color(color) {}
-};
-
-std::vector<QColor> colors = {QColor(255, 165, 0), QColor(21, 171, 0), QColor(255, 0, 255), Qt::blue, Qt::green, Qt::red};
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -36,71 +27,43 @@ int main(int argc, char *argv[])
         printUsage();
         return 0;
     }
-    std::unordered_set<std::string> files;
+    std::vector<std::string> files;
     for (int i = 1; i < argc; ++i)
     {
-        files.emplace(argv[i]);
+        files.emplace_back(argv[i]);
     }
-    using uint = uint32_t;
-    std::vector<Table> results;
 
-    uint minN = static_cast<uint>(INT_MAX);
-    uint maxN = 0;
-    uint minAmount = static_cast<uint>(INT_MAX);
-    uint maxAmount = 0;
-    std::ifstream fin;
-    srand(time(nullptr));
-    uint colorCnt = 0;
-    for (const auto &name : files)
+    auto results = utils::parseResultsFiles(files);
+    std::pair<uint, uint> minMaxN;
+    std::pair<uint, uint> minMaxAmount;
+    auto update = [](std::pair<uint, uint> oldMinMax,
+                     std::pair<uint, uint> newMinMaxN) -> std::pair<uint, uint>
     {
-        QColor color ;
-        if (colorCnt >= colors.size())
-            color = QColor(rand() % 245 + 10, rand() % 245 + 10, rand() % 245 + 10);
-        else color = colors[colorCnt++];
-        Table t(name, color);
-        fin.open(name);
-        if (!fin.good())
-        {
-            std::string s("no such file ");
-            s += name;
-            printUsage(s);
-            return 0;
-        }
-        std::string str;
-        std::getline(fin, str);
-        uint lambda, n, amount;
-        double deviation;
-        while (fin >> lambda >> n >> amount >> deviation)
-        {
-            t.data[lambda][n] = std::make_pair(amount, deviation);
-        }
-        for (const auto &mpLambda : t.data)
-        {
-            minN = std::min(mpLambda.second.begin()->first, minN);
-            maxN = std::max((--mpLambda.second.end())->first, maxN);
-            for (const auto &mpN : mpLambda.second)
-            {
-                minAmount = std::min(mpN.second.first, minAmount);
-                maxAmount = std::max(mpN.second.first, maxAmount);
-            }
-        }
-        results.emplace_back(std::move(t));
-        fin.close();
+        return {std::min(oldMinMax.first, newMinMaxN.first), std::max(oldMinMax.second, newMinMaxN.second)};
+    };
+    for (const auto &table : results)
+    {
+        minMaxN = update(minMaxN, table.getMinMaxN());
+        minMaxAmount = update(minMaxAmount, table.getMinMaxAmount());
     }
 
-    MainWindow w({minN, maxN + 100}, {minAmount, maxAmount + 100});
-    for (auto it1 = results[0].data.begin(); it1 != results[0].data.end(); ++it1)
+    MainWindow w({minMaxN.first, minMaxN.second + 100}, {minMaxAmount.first, minMaxAmount.second + 100});
+    auto dataRes0 = results[0].getData();
+    for (auto it1 = dataRes0.begin(); it1 != dataRes0.end(); ++it1)
     {
         uint curLambda = it1->first;
         for (auto it2 = results.begin(); it2 != results.end(); ++it2)
         {
-            const std::map<uint, std::pair<uint, double>> &nToAmountDev = it2->data[curLambda];
+            auto dataIt2 = it2->getData();
+            const auto &nToAmountDev = dataIt2[curLambda];
             std::vector<std::pair<uint, uint>> v;
             for (const auto &n : nToAmountDev)
             {
                 v.push_back(std::make_pair(n.first, n.second.first));
             }
-            w.addNewGraph(v, it2->color, it2->name.c_str());
+            std::sort(v.begin(), v.end(),
+            [](const std::pair<uint, uint> &a, const std::pair<uint, uint> &b) { return a.first < b.first; });
+            w.addNewGraph(v, it2->getColor(), it2->getFileName().c_str());
         }
         w.saveGraphPng(curLambda);
         w.removeAllGraphs();
