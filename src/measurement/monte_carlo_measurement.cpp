f744ace115777250fb98fa993d6dc7f8cf9603a8
kant97 <kirant9797@gmail.com>
2018-04-04 23:17:44 +0300

    Monte Carlo method for achieving optimal parameters

---
+++ src/measurement/monte_carlo_measurement.cpp
@@ -0,0 +1,34 @@
+#include "configParser.h"
+#include "measurer.h"
+#include <iostream>
+#include <stdlib.h>
+#include "logger.h"
+
+int main()
+{
+    ParsedParams params;
+    params.n[0] = 10000;
+    params.n[1] = 100000;
+    params.n[2] = 10000;
+    params.lambda[0] = 1600;
+    params.lambda[1] = 1600;
+    params.lambda[2] = 1;
+    params.tests = 10;
+    params.wantedAlgos = {{"A3", "resultsSelfAdjustingThree.txt"}};
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis1(1.1, 2.1);
+    std::uniform_real_distribution<double> dis2(0.2, 0.8);
+
+    const int N = 50;
+    for (int i = 1; i <= N; ++i)
+    {
+        LOG("Test number:", i, "out of:", N);
+        params.selfAdjParams = {dis1(gen), 1., dis2(gen)};
+        Measurer m(std::make_shared<const ParsedParams>(params));
+        m.run();
+        system("addParam");
+    }
+    return 0;
+}
